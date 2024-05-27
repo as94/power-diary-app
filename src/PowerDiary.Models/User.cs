d791fea96c0ef6f296c81bee46b120eed1428991
anatolysorokin <anatoly.sorokin.personal@gmail.com>
2024-05-27 10:52:23 +0500

    add user model

---
+++ src/PowerDiary.Models/User.cs
@@ -0,0 +1,13 @@
+namespace PowerDiary.Models;
+
+public sealed class User
+{
+    public User(Guid id, string name)
+    {
+        Id = id;
+        Name = name;
+    }
+
+    public Guid Id { get; }
+    public string Name { get; }
+}
